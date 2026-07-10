bdf50880bb1b988ca4e763874be4b8b7dcd38f0c
Schneider-sizeof <ayyoubberoigui@gmail.com>
2026-07-11 00:27:00 +0200

    Fix Django template Context copy compatibility bug on Python 3.14+ in apps/core/apps.py

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -5,3 +5,25 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.core'
     verbose_name = 'Core & Pages'
+
+    def ready(self):
+        import sys
+        if sys.version_info >= (3, 14):
+            try:
+                from django.template import Context, BaseContext
+                
+                def new_base_context_copy(self):
+                    dup = self.__class__()
+                    dup.dicts = [d.copy() for d in self.dicts]
+                    return dup
+
+                def new_context_copy(self):
+                    dup = self.__class__()
+                    dup.dicts = self.dicts[:]
+                    return dup
+
+                BaseContext.__copy__ = new_base_context_copy
+                Context.__copy__ = new_context_copy
+            except ImportError:
+                pass
+
